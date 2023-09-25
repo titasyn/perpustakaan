c41545ee2e0ca7006fae51cc019f387319d89ba4
titasyn <titasyn@gmail.com>
2023-09-25 16:55:40 +0700

    Latihan1

---
+++ application/controllers/latihan1.php
@@ -0,0 +1,15 @@
+<?php
+class latihan1 extends CI_Controller
+{
+    public function index () 
+    {
+        echo "<h1>Biodata</h1>";
+        echo "Nama saya Tita Suryani
+        <p>NIM : 19220764</p>
+        <p>No. Hp : 083102202143</p>
+        <p>Kelas : 19.3A.24</p>
+        <p>Hobi : Menggambar</p>
+        <p>Alamat : Cikarang Utara, Bekasi</p>
+        <p>Umur : 19 Tahun</p>";
+    }
+}
